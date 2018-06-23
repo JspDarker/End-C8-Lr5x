1c4b02ea8ebb48baa0eecf075a6f63b6ac638578
Nhat Thanh <thanhdntage@gmail.com>
2018-06-23 20:50:32 +0700

    includes lib client

---
+++ resources/views/includes/nav-bar.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: storm
+ * Date: 23/06/2018
+ * Time: 20:49
+ */
---
+++ resources/views/kye.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title','App@Blade')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-center bg-success">This is Enl</h1>
+    </div>
+@endsection
+
